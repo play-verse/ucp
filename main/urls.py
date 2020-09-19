2c82c9640babe32b9dcbc698ff7435af3082f92a
Nathan Ramli <hezkyanatanael84@gmail.com>
2020-09-19 15:06:14 +0700

    fix major bugs

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     path('login/', views.login),
+    path('logout/', views.logout, name="logout"),
     path('', views.index, name='index'),
 ]
--- main/views.py
+++ main/views.py
@@ -6,14 +6,18 @@ from .models import Snippet
 from .forms import LoginForm
 
 def index(request):
+    # Cek jika user belum login
+    if not isSudahLogin(request):
+        return HttpResponseRedirect('/logout/')
+
     context = {
-        "judul": "Judul",
+        "judul": "Home - Playverse UCP",
     }
     return render(request, "index.html", context)
 
 def login(request):
-
-    if 'uid' in request.session:
+    # Cek jika user belum login
+    if isSudahLogin(request):
         return HttpResponseRedirect('/')
 
     if request.method == 'POST':
@@ -36,16 +40,33 @@ def login(request):
                 request.session['uid'] = result[0]['uid']
                 request.session['username'] = result[0]['username']
                 request.session['avatar'] = result[0]['avatar']
+                request.session['email'] = result[0]['email']
                 return HttpResponseRedirect('/')
             else:
-                # for val in request.session:
-                #     return HttpResponse(val)
                 return HttpResponseRedirect('/login/')
     else:
         form = LoginForm()
 
     context = {
-        "judul": "Login",
+        "judul": "Login - Playverse UCP",
         "form" : form
     }
     return render(request, "login.html", context)
+
+def logout(request):
+    # Cek setiap session apakah None?
+    if request.session.get("uid") != None:
+        del request.session['uid']
+    if request.session.get("username") != None:
+        del request.session['username']
+    if request.session.get("avatar") != None:
+        del request.session['avatar']
+    if request.session.get("email") != None:
+        del request.session['email']
+    return HttpResponseRedirect('/login/')
+
+def isSudahLogin(request):
+    return not(request.session.get("uid") == None or \
+        request.session.get("username") == None or \
+        request.session.get("avatar") == None or \
+        request.session.get("email") == None)
